1112e770d9ed2ff6a83b66eb5d7470c95f07fd49
Meghan <meghan.leah.clark@gmail.com>
2017-10-23 15:46:39 -0700

    added some comments and cleaned things up for readability

--- raw/lstm_even_odd_example.py
+++ raw/lstm_even_odd_example.py
@@ -11,16 +11,14 @@ model = Sequential()
 model.add(LSTM(32, return_sequences=True,
                input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
 model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
-#model.add(LSTM(32))  # return a single vector of dimension 32
 model.add(Dense(2, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-
-#x_train = np.random.random((1000, timesteps, data_dim))
-#y_train = np.random.random((1000, num_classes))
+# Generate training data
+# dimensions are batch_size, timesteps, data_sim
 x_train = np.array([[[0, 0, 0, 1]],
                    [[0, 0, 1, 0]],
                    [[1, 0, 0, 0]],
@@ -29,11 +27,18 @@ x_train = np.array([[[0, 0, 0, 1]],
                    [[0, 1, 0, 1]],
                    [[1, 1, 0, 1]],
                    [[0, 0, 1, 1]]])
-y_train = np.array([[[0, 1]], [[1, 0]], [[1, 0]], [[1, 0]], [[1, 0]], [[0, 1]],[[0, 1]],[[0, 1]]])
+# two classes: [0,1] => "odd", [1,0] => "even"
+y_train = np.array([[[0, 1]],
+                    [[1, 0]],
+                    [[1, 0]],
+                    [[1, 0]],
+                    [[1, 0]],
+                    [[0, 1]],
+                    [[0, 1]],
+                    [[0, 1]]])
 
-# Generate dummy validation data
-#x_val = np.random.random((100, timesteps, data_dim))
-#y_val = np.random.random((100, num_classes))
+# Generate validation data
+# dimensions are batch_size, timesteps, data_sim
 x_val = np.array([[[0, 0, 0, 0]],
                  [[1, 1, 1, 1]],
                  [[1, 1, 0, 0]],
@@ -42,8 +47,14 @@ x_val = np.array([[[0, 0, 0, 0]],
                  [[0, 1, 1, 0]],
                  [[1, 0, 0, 1]],
                  [[1, 0, 1, 1]]])
-y_val = np.array([[[1,0]], [[0,1]], [[1,0]], [[0,1]], [[1,0]], [[1,0]], [[0,1]], [[0,1]]])
-
+y_val = np.array([[[1,0]],
+                  [[0,1]],
+                  [[1,0]],
+                  [[0,1]],
+                  [[1,0]],
+                  [[1,0]],
+                  [[0,1]],
+                  [[0,1]]])
 
 model.fit(x_train, y_train,
           batch_size=8, epochs=8,
