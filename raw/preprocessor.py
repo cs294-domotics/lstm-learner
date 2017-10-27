33bbe0214c592603d320294f02e5cbfc8e562ff0
Meghan <meghan.leah.clark@gmail.com>
2017-10-27 11:27:48 -0700

    prints out complete feature vector set for any CASAS data file - need to get the label vectors and save input and labels to file

--- raw/preprocessor.py
+++ raw/preprocessor.py
@@ -1,25 +1,23 @@
 import numpy as np
 
-filename = "../data/stupid_simple"
+filename = "../data/simple"
+desired_device_types = ['M', 'D'] #only motion and door sensors will be used in feature vector
 
 def main():
     with open(filename) as f:
 		data = f.read().splitlines()
     num_events = len(data)
     device_type_counts, device_type_buckets = get_devices(data)
-    print(num_events)
-    print(device_type_counts)
     # for each timestep, assemble input vector
-    input_types = ['M', 'D'] #only motion and door sensors will be used in feature vector
     input_vectors = []
-    input_vector, device_indices = initialize_vector(device_type_counts, device_type_buckets, input_types)
+    input_vector, device_indices = initialize_vector(device_type_counts, device_type_buckets, desired_device_types)
     for t in range(num_events):
-        if is_well_formed(data[t]) and get_device_type(get_device(data[t])) in input_types:
+        if is_well_formed(data[t]) and get_device_type(get_device(data[t])) in desired_device_types:
             #get new device/value
             device = get_device(data[t])
             value = get_value(data[t])
             #update input vector at device index to reflect newest value
-            input_vector[device_indices[device]] = value
+            input_vector[device_indices[device]] = encode(value)
             #add input vector to input vectors
             input_vectors.append(input_vector)
             print(input_vector)
@@ -56,18 +54,25 @@ def get_value(line):
 def get_activity(line):
     return line.split()[4]
 
-def initialize_vector(device_type_counts, device_type_buckets, input_types):
+def encode(value):
+    encoding = {'OFF': 0,
+                'ON':  1,
+                'CLOSE': 0,
+                'OPEN': 1}
+    return encoding[value]
+
+def initialize_vector(device_type_counts, device_type_buckets, desired_device_types):
     # figure out how many features the input vector will have
     num_features = 0
     for device_type in device_type_counts:
-        if device_type in input_types:
+        if device_type in desired_device_types:
             num_features += device_type_counts[device_type]
 
     # assign an index to each device
     devices = []
     device_indices = {}
     for device_type in device_type_buckets:
-        if device_type in input_types:
+        if device_type in desired_device_types:
             devices += device_type_buckets[device_type]
     i = 0
     for d in devices:
