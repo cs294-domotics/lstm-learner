2e4209469c658ca6eb3d28680b13ab4f545da9b8
Meghan <meghan.leah.clark@gmail.com>
2017-10-27 12:22:51 -0700

    now generates the input and label vectors - need to add in the ability to cut off the initial part before all labels have been seen at least once, and then save it all to a file

--- raw/preprocessor.py
+++ raw/preprocessor.py
@@ -1,42 +1,64 @@
 import numpy as np
 
-filename = "../data/simple"
-desired_device_types = ['M', 'D'] #only motion and door sensors will be used in feature vector
+filename = "../data/twor2010"
+desired_input_types = ['M', 'D'] #only motion and door sensors will be used in feature vector
+desired_label_types = ['L'] #predicting light states
 
 def main():
     with open(filename) as f:
 		data = f.read().splitlines()
     num_events = len(data)
-    device_type_counts, device_type_buckets = get_devices(data)
-    # for each timestep, assemble input vector
+    # get list of all devices in dataset sorted into buckets by type
+    device_buckets = get_devices(data)
+    # split into input and output devices
+    input_device_buckets, label_device_buckets = filter_devices(device_buckets)
+    # initialize input and label vectors
     input_vectors = []
-    input_vector, device_indices = initialize_vector(device_type_counts, device_type_buckets, desired_device_types)
-    for t in range(num_events):
-        if is_well_formed(data[t]) and get_device_type(get_device(data[t])) in desired_device_types:
-            #get new device/value
-            device = get_device(data[t])
-            value = get_value(data[t])
-            #update input vector at device index to reflect newest value
-            input_vector[device_indices[device]] = encode(value)
-            #add input vector to input vectors
-            input_vectors.append(input_vector)
-            print(input_vector)
+    label_vectors = []
+    input_vector, input_device_indices = initialize_vector(input_device_buckets)
+    label_vector, label_device_indices = initialize_vector(label_device_buckets)
+    # for each timestep, update either input vector or label vector or neither (if a device type we're ignoring)
+    for line in data:
+        if is_well_formed(line):
+            device = get_device(line)
+            value = get_value(line)
+            device_type = get_device_type(device)
+            if device_type in desired_input_types or device_type in desired_label_types:
+                if device_type in desired_input_types:
+                    input_vector[input_device_indices[device]] = encode(value)
+                elif device_type in desired_label_types:
+                    label_vector[label_device_indices[device]] = encode(value)
+                input_vectors.append(input_vector)
+                label_vectors.append(label_vector)
+                print(input_vector),
+                print(label_vector)
 
+# returns list of devices sorted into buckets by type
+# eg, {'M': ['M001', 'M002'],
+#      'D': ['D001']}
 def get_devices(data):
-    device_type_buckets = {}
+    device_buckets = {}
     for line in data:
         if is_well_formed(line):
             device = get_device(line)
             device_type = get_device_type(device)
-            if device_type not in device_type_buckets:
-                device_type_buckets[device_type] = [device]
+            if device_type not in device_buckets:
+                device_buckets[device_type] = [device]
             else:
-                if device not in device_type_buckets[device_type]:
-                    device_type_buckets[device_type].append(device)
-    device_type_counts = {}
-    for device_type in device_type_buckets:
-        device_type_counts[device_type] = len(device_type_buckets[device_type])
-    return device_type_counts, device_type_buckets
+                if device not in device_buckets[device_type]:
+                    device_buckets[device_type].append(device)
+    return device_buckets
+
+def filter_devices(device_buckets):
+    input_device_buckets = {}
+    label_device_buckets = {}
+    for device_type in device_buckets:
+        if device_type in desired_input_types:
+            input_device_buckets[device_type] = device_buckets[device_type]
+        elif device_type in desired_label_types:
+            label_device_buckets[device_type] = device_buckets[device_type]
+    return input_device_buckets, label_device_buckets
+
 
 def is_well_formed(line):
     fields = line.split()
@@ -59,27 +81,28 @@ def encode(value):
                 'ON':  1,
                 'CLOSE': 0,
                 'OPEN': 1}
-    return encoding[value]
+    try:
+        encoded_value = encoding[value]
+    except KeyError:
+        try:
+            encoded_value = float(value)
+        except ValueError:
+            encoded_value = value
+    return encoded_value
 
-def initialize_vector(device_type_counts, device_type_buckets, desired_device_types):
+def initialize_vector(device_buckets):
     # figure out how many features the input vector will have
     num_features = 0
-    for device_type in device_type_counts:
-        if device_type in desired_device_types:
-            num_features += device_type_counts[device_type]
-
-    # assign an index to each device
-    devices = []
     device_indices = {}
-    for device_type in device_type_buckets:
-        if device_type in desired_device_types:
-            devices += device_type_buckets[device_type]
-    i = 0
-    for d in devices:
-        device_indices[d] = i
-        i+=1
+    index = 0
+    for device_type in device_buckets:
+        num_features += len(device_buckets[device_type])
+        # assign an index to each device
+        for device in device_buckets[device_type]:
+            device_indices[device] = index
+            index += 1
 
-    # create a default input vector
+    # create a default vector
     initial_vector = []
     for i in range(num_features):
         initial_vector.append(0)
