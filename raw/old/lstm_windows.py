8cec8d057854f555082c72465c2b3d2a188c783b
Meghan <meghan.leah.clark@gmail.com>
2017-12-08 15:48:24 -0800

    no longer balances the classes, just reads in different files for training and testing that have already been balanced

--- raw/old/lstm_windows.py
+++ raw/old/lstm_windows.py
@@ -6,95 +6,96 @@ from keras.optimizers import RMSprop
 from keras import regularizers
 import numpy as np
 
-features_filename = "build/L005_features.npy"
-labels_filename = "build/L005_labels.npy"
+#features_filename = "build/events/raw/no_light_no_time/L005_20_features.npy"
+#labels_filename = "build/events/raw/no_light_no_time/L005_20_labels.npy"
+#features_filename = "build/events/raw/light_and_time/L005_20_features.npy"
+#labels_filename = "build/events/raw/light_and_time/L005_20_labels.npy"
+#features_filename = "build/events/activities/no_light_no_time/L005_20_features.npy"
+#labels_filename = "build/events/activities/no_light_no_time/L005_20_labels.npy"
+#features_filename = "build/events/activities/light_and_time/L005_20_features.npy"
+#labels_filename = "build/events/activities/light_and_time/L005_20_labels.npy"
+#features_filename = "build/events/activities/light_and_time/L005_40_features.npy"
+#labels_filename = "build/events/activities/light_and_time/L005_40_labels.npy"
+
+load_folder = "build/events/activities/light_and_time/"
+
+##### TRAIN
+
+#input_train_filename = load_folder + "L005_40_one_day_train_features.npy"
+#output_train_filename = load_folder + "L005_40_one_day_train_labels.npy"
+
+#input_train_filename = load_folder + "L005_40_one_week_train_features.npy"
+#output_train_filename = load_folder + "L005_40_one_week_train_labels.npy"
+
+#input_train_filename = load_folder + "L005_40_two_weeks_train_features.npy"
+#output_train_filename = load_folder + "L005_40_two_weeks_train_labels.npy"
+
+input_train_filename = load_folder + "L005_40_one_month_train_features.npy"
+output_train_filename = load_folder + "L005_40_one_month_train_labels.npy"
+
+##### TESTS
+
+#input_test_filename = load_folder + "L005_40_one_day_test_features.npy"
+#output_test_filename = load_folder + "L005_40_one_day_test_labels.npy"
+
+#input_test_filename = load_folder + "L005_40_one_week_test_features.npy"
+#output_test_filename = load_folder + "L005_40_one_week_test_labels.npy"
+
+#input_test_filename = load_folder + "L005_40_two_weeks_test_features.npy"
+#output_test_filename = load_folder + "L005_40_two_weeks_test_labels.npy"
+
+input_test_filename = load_folder + "L005_40_one_month_test_features.npy"
+output_test_filename = load_folder + "L005_40_one_month_test_labels.npy"
 
 
 def main():
+
     print("loading data...")
-    features = np.load(features_filename)
-    labels = np.load(labels_filename)
 
-    turn_on_percent = round((np.sum(labels[:,1])/len(labels[:,1]))*100, 2)
-    turn_off_percent = round((np.sum(labels[:,0])/len(labels[:,0]))*100, 2)
-    nop_percent = round((np.sum(labels[:,2])/len(labels[:,2]))*100, 2)
-    print("   light turns on " + str(turn_on_percent) + " percent of the time")
-    print("   light turns off " + str(turn_off_percent) + " percent of the time")
-    print("   light does nothing " + str(nop_percent) + " percent of the time")
-    print("   accuracy must be higher than " + str(round(max(turn_on_percent, turn_off_percent, nop_percent)/100.0, 4)))
+    x_train = np.load(input_train_filename)
+    y_train = np.load(output_train_filename)
 
+    x_val = np.load(input_test_filename)
+    y_val = np.load(output_test_filename)
+
+    # turn_on_percent = round((np.sum(labels[:,1])/len(labels[:,1]))*100, 2)
+    # turn_off_percent = round((np.sum(labels[:,0])/len(labels[:,0]))*100, 2)
+    # nop_percent = round((np.sum(labels[:,2])/len(labels[:,2]))*100, 2)
+    # print("   light turns on " + str(turn_on_percent) + " percent of the time")
+    # print("   light turns off " + str(turn_off_percent) + " percent of the time")
+    # print("   light does nothing " + str(nop_percent) + " percent of the time")
+    # print("   accuracy must be higher than " + str(round(max(turn_on_percent, turn_off_percent, nop_percent)/100.0, 4)))
+    #
     print("data loaded...")
 
-    if len(features) != len(labels):
-        print("uh oh...features and labels are different sizes.")
-
-    print("generating training data...")
-
-    data_dim = len(features[0][0])
-    num_classes = len(labels[0])
-    num_samples = len(features)
-    timesteps = len(features[0]) #number of timesteps per batch
-
-
-    class_features = []
-    for i in range(num_classes):
-        class_features.append([])
-    class_labels = []
-    for i in range(num_classes):
-        class_labels.append([])
-
-    for i in range(len(features)):
-        for j in range(num_classes):
-            if labels[i][j] == 1:
-                class_labels[j].append(labels[i])
-                class_features[j].append(features[i])
-
-    # Generate test data
-    # dimensions are batch_size, timesteps, data_sim
-    x_train = []
-    for class_examples in class_features:
-        half = int(len(class_examples)/2)
-        x_train += class_examples[:half]
-    x_train = np.array(x_train)
-    y_train = []
-    for class_examples in class_labels:
-        half = int(len(class_examples)/2)
-        y_train += class_examples[:half]
-    y_train = np.array(y_train)
-    print(features.shape)
+    print(x_train.shape)
+    print(x_val.shape)
     print(y_train.shape)
-
-
-    turn_on_percent = round((np.sum(y_train[:,1])/len(y_train[:,1]))*100, 2)
-    turn_off_percent = round((np.sum(y_train[:,0])/len(y_train[:,0]))*100, 2)
-    nop_percent = round((np.sum(y_train[:,2])/len(y_train[:,2]))*100, 2)
-    print("   light turns on " + str(turn_on_percent) + " percent of the time")
-    print("   light turns off " + str(turn_off_percent) + " percent of the time")
-    print("   light does nothing " + str(nop_percent) + " percent of the time")
-    print("   accuracy must be higher than " + str(round(max(turn_on_percent, turn_off_percent, nop_percent)/100.0, 4)))
-
-    print("generating test data...")
-    # Generate validation data
-    # dimensions are batch_size, timesteps, data_sim
-    x_val = []
-    for class_examples in class_features:
-        half = int(len(class_examples)/2)
-        x_val += class_examples[half:]
-    x_val = np.array(x_val)
-    y_val = []
-    for class_examples in class_labels:
-        half = int(len(class_examples)/2)
-        y_val += class_examples[half:]
-    y_val = np.array(y_val)
     print(y_val.shape)
 
-    turn_on_percent = round((np.sum(y_val[:,1])/len(y_val[:,1]))*100, 2)
-    turn_off_percent = round((np.sum(y_val[:,0])/len(y_val[:,0]))*100, 2)
-    nop_percent = round((np.sum(y_val[:,2])/len(y_val[:,2]))*100, 2)
-    print("   light turns on " + str(turn_on_percent) + " percent of the time")
-    print("   light turns off " + str(turn_off_percent) + " percent of the time")
-    print("   light does nothing " + str(nop_percent) + " percent of the time")
-    print("   accuracy must be higher than " + str(round(max(turn_on_percent, turn_off_percent, nop_percent)/100.0, 4)))
+    if len(x_train) != len(y_train):
+        print("uh oh...features and labels are different sizes.")
+
+    data_dim = len(x_train[0][0])
+    num_classes = len(y_train[0])
+    num_samples = len(x_train)
+    timesteps = len(x_train[0]) #number of timesteps per batch
+
+    # turn_on_percent = round((np.sum(y_train[:,1])/len(y_train[:,1]))*100, 2)
+    # turn_off_percent = round((np.sum(y_train[:,0])/len(y_train[:,0]))*100, 2)
+    # nop_percent = round((np.sum(y_train[:,2])/len(y_train[:,2]))*100, 2)
+    # print("   light turns on " + str(turn_on_percent) + " percent of the time")
+    # print("   light turns off " + str(turn_off_percent) + " percent of the time")
+    # print("   light does nothing " + str(nop_percent) + " percent of the time")
+    # print("   accuracy must be higher than " + str(round(max(turn_on_percent, turn_off_percent, nop_percent)/100.0, 4)))
+
+    # turn_on_percent = round((np.sum(y_val[:,1])/len(y_val[:,1]))*100, 2)
+    # turn_off_percent = round((np.sum(y_val[:,0])/len(y_val[:,0]))*100, 2)
+    # nop_percent = round((np.sum(y_val[:,2])/len(y_val[:,2]))*100, 2)
+    # print("   light turns on " + str(turn_on_percent) + " percent of the time")
+    # print("   light turns off " + str(turn_off_percent) + " percent of the time")
+    # print("   light does nothing " + str(nop_percent) + " percent of the time")
+    # print("   accuracy must be higher than " + str(round(max(turn_on_percent, turn_off_percent, nop_percent)/100.0, 4)))
 
     print("building model...")
 
@@ -112,7 +113,7 @@ def main():
 
     print("training and testing model...")
     model.fit(x_train, y_train,
-              batch_size=8, epochs=50,
+              batch_size=8, epochs=5,
               validation_data=(x_val, y_val))
 
 main()
