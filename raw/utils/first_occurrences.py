1d9d5884f293ff54fe0118cac8a2137b44b80d22
Meghan <meghan.leah.clark@gmail.com>
2017-12-08 15:41:01 -0800

    finds first occurrences of devices in a CASAS dataset

--- raw/utils/first_occurrences.py
+++ raw/utils/first_occurrences.py
@@ -1,11 +1,14 @@
 # This script prints out the first occurrence of each device in the dataset
 
-filename = "../data/twor2010"
+filename = "../../data/twor2010"
+
+DESIRED_TYPES = ['L', 'D', 'M']
 
 def main():
     with open(filename) as f:
-		data = f.read().splitlines()
-    device_type_buckets, device_first_occurrences = find_devices(data)
+        data = f.read().splitlines()
+    device_type_buckets, device_first_occurrences, device_count = find_devices(data)
+    print(device_count)
     for device_type in device_type_buckets:
         devices = device_type_buckets[device_type]
         for device in devices:
@@ -15,18 +18,22 @@ def main():
 def find_devices(lines):
     device_type_buckets = {}
     device_first_occurrences = {}
+    device_count = 0
     for line in lines:
         if is_well_formed(line):
             device = get_device(line)
             device_type = get_device_type(device)
-            if device_type not in device_type_buckets:
-                device_type_buckets[device_type] = [device]
-                device_first_occurrences[device] = get_timestamp(line)
-            else:
-                if device not in device_type_buckets[device_type]:
-                    device_type_buckets[device_type].append(device)
+            if device_type in DESIRED_TYPES:
+                if device_type not in device_type_buckets:
+                    device_type_buckets[device_type] = [device]
                     device_first_occurrences[device] = get_timestamp(line)
-    return device_type_buckets, device_first_occurrences
+                    device_count += 1
+                else:
+                    if device not in device_type_buckets[device_type]:
+                        device_type_buckets[device_type].append(device)
+                        device_first_occurrences[device] = get_timestamp(line)
+                        device_count += 1
+    return device_type_buckets, device_first_occurrences, device_count
 
 def is_well_formed(line):
     fields = line.split()
