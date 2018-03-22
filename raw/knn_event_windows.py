e170d3491a91388ab805f27401e1c3c24dda6262
Sarah Sun <sarah.sun123@outlook.com>
2018-03-22 16:46:24 -0700

    checked if knn_events correctly implements euclidean distance, but need to check more stuff on TODO list

--- raw/knn_event_windows.py
+++ raw/knn_event_windows.py
@@ -2,6 +2,13 @@
 
 # Heavily borrowed from https://kevinzakka.github.io/2016/07/13/k-nearest-neighbor/
 
+"""
+TODO: (1) check if euclidean distance is accurate on trained values using k = 1 and an accuracy function
+      (2) incorporate numpy arrays better
+      (3) try to optimize code
+      (4) maybe used weighted knn bc right now the k nearest neighbors have equal weight, but what happens if some further neighbors skew value to inaccurate value
+      (5) using most_common in the collections counter library chooses arbitrarily in case of ties (i.e. 2 things appear the same amount of times, most_common arbitrarily chooses one)
+"""
 import numpy as np
 from numpy import linalg as LA
 from collections import Counter
@@ -56,9 +63,11 @@ def main():
 
     print("loading data...")
 
+    # Training Data
     x_train = np.load(input_train_filename)
     y_train = np.load(output_train_filename)
 
+    # Testing Data
     x_val = np.load(input_test_filename)
     y_val = np.load(output_test_filename)
 
@@ -80,32 +89,34 @@ def main():
     print("building model...")
 
     # TRAIN KNN HERE
+    
     """
-    print(type(x_train))
-    print("First element of y_train: ", y_train[0][0], "First row of second element of y_train: ", y_train[1][0], " First Element of y_train: ", y_train[0:2])
-    print("Distance of first row of x_train1 and x_train2): ", row_vector_distance(y_train[0], y_train[1]))
+    #print(type(x_train))
+    #print("First element of y_train: ", y_train[0][0], "First row of second element of y_train: ", y_train[1][0], " First Element of y_train: ", y_train[0:2])
+    #print("Distance of first row of x_train1 and x_train2): ", row_vector_distance(y_train[0], y_train[1]))
+    
+    #TESTS TO SEE IF matrices_distance and row_vector_distance WORK
     test1 = np.zeros((4, 4))
     test2 = np.ones((4, 4))
-    print(len(test1.shape))
+    test2 = np.add(test2, test2)
+    print((test1.shape), " ", len(test1.shape))
     print("distance between np array of 0s and 1s: ", row_vector_distance(test1, test2))
     print("matrix distance between np array of 0s and 1s: ", matrices_distance(test1, test2))
     
     """
-    #print("First element of x_val: ", x_val[0:3], " First Element of y_val: ", y_val[0:3])
-    #print("Length of first element of x_train: ", matrix_distance(x_train[0]))
-    #print("Length of third element of x_train: ", matrix_distance(x_train[2]))
-    prediction = predict(x_train, y_train, x_val[0], 3)
+    start = 300
+
+    print(start, " index element of x_val: ", x_train[start], start, " index element of y_val: ", y_train[start])
+    prediction = predict(x_train, y_train, x_train[start], 3)
     print(prediction)
-    predictions = kNearestNeighbor(x_train, y_train, x_val[0:3], 3)
-    print("First three actual predictions: ", predictions )
-    print("First three expected predictions: ", y_val[0:3])
-    """
+    #print(y_val) 
+    #Tests for kNearestNeighbor
+
+    #predictions = kNearestNeighbor(x_train, y_train, x_val[0:3], 3)
+    #print("First three actual predictions: ", predictions )
+    #print("First three expected predictions: ", y_val[0:3])
 
     """
-    # Using sklearn.neighbors KNN algorithm, doesn't work bc dimensions off
-    #instantiate Learning Model using sklearn.neighbors
-    knn = KNeighborsClassifier(n_neighbors = 5)
-
     # fitting the model
     knn.fit(x_train, y_train)
 
@@ -118,6 +129,7 @@ def main():
 
     print("Accuracy score: ", accuracy_score(y_val, pred))
     """
+
 def kNearestNeighbor(X_train, y_train, X_test, k):
     # train on input data
 
@@ -131,6 +143,11 @@ def kNearestNeighbor(X_train, y_train, X_test, k):
 
 
 # Function that predicts where an unsigned label will go based on training data
+""" Parameters: X_train: entire x_train matrix that we train over
+y_train: entire y_train matrix we train over
+x_test: single x_matrix we test
+k: number of nearest neighbors we use to decide what prediction x_test will be (MUST BE ODD)
+"""
 def predict(X_train, y_train, x_test, k):
     distances = []
     targets = []
@@ -140,16 +157,22 @@ def predict(X_train, y_train, x_test, k):
         # TODO: Want to update this so over all predicts, we have already calculated the distance of each matrix from a "0" matrix and then we only need to subtract the difference between each matrix here
         dist = matrices_distance(X_train[i,:], x_test)
         distances.append([dist, i])
+    #print(distances)
 
-    # sort the list of distances
+    # sort the list of distances based on first element of each in ASC order
     distances = sorted(distances)
 
-    # make a list of the k neighbors' targets
+    #print(y_train)
+    #print (distances[:10])
+    # make a list of indices of the k neighbors' targets
     for i in range(k):
         index = distances[i][1]
         targets.append(tuple(y_train[index,:].tolist()))
-
+    
+    #print(targets)
     # return most common target
+    # TODO: bug, if more than one thing appears at the same frequency, arbitrarily chooses one
+    # Probably want to weight it so the one with the closest distance is picked instead
     return list(Counter(targets).most_common(1)[0][0])
 
 # Calculates the Euclidean distance btween two 2d numpy arrays
@@ -157,22 +180,32 @@ def matrices_distance(m1, m2):
     # if the two matrices are not the same shape, cannot compute the difference in size
     if m1.shape != m2.shape:
         print("ERROR: Cannot calculate distance bewteen matrices of different sizes")
-    else:
-        # if the matrices are 1d np arrays, calculate the Euclidean difference of the row
-        if len(m1.shape) == 1:
-            return row_vector_distance(m1, m2)
+        return
+    # if the matrices are 1d np arrays, calculate the Euclidean difference of the row
+    if len(m1.shape) == 1:
+        return row_vector_distance(m1, m2)
 
-        # calculate the sum of the euclidean distances of each row
-        else:
-            vector_distances = []
-            for i in range(m1.shape[0]):
-                print(i)
-                row_dist = row_vector_distance(m1[i,:], m2[i,:])
-                vector_distances.append(row_dist)
-            return sum(vector_distances)
+    # calculate the sum of the euclidean distances of each row
+    else:
+        vector_distances = []
+        for i in range(m1.shape[0]):
+            row_dist = row_vector_distance(m1[i,:], m2[i,:])
+            vector_distances.append(row_dist)
+        return sum(vector_distances)
+
+def matrix_distance(m1):
+    return matrices_distance(m1, np.zeros(m1.shape)) 
 
 # Calculates the Euclidean distance between two 1d numpy arrays
 def row_vector_distance(v1, v2):
+    #print(v1)
+    #print(v2)
+    if v1.shape != v2.shape:
+        print("ERROR: cannot calculate distance between vectors of different sizes")
+        return
+    if len(v1.shape) != 1:
+        print("ERROR: thing passed in is not a vector")
+        return
     return np.sqrt(np.sum((v1 - v2) ** 2))
 
 """
